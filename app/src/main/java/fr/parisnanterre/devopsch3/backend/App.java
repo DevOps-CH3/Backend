8914a8513dcac91836bb9d2b4283f8e44fbaae3e
shan92i <ouragheneh@gmail.com>
2024-10-18 15:32:48 +0200

    add spring

--- app/src/main/java/fr/parisnanterre/devopsch3/backend/App.java
+++ app/src/main/java/fr/parisnanterre/devopsch3/backend/App.java
@@ -3,12 +3,13 @@
  */
 package fr.parisnanterre.devopsch3.backend;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
     }
 }
--- app/src/test/java/fr/parisnanterre/devopsch3/backend/AppTest.java
+++ app/src/test/java/fr/parisnanterre/devopsch3/backend/AppTest.java
@@ -3,12 +3,8 @@
  */
 package fr.parisnanterre.devopsch3.backend;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+    
 }
